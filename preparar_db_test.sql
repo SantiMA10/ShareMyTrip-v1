f9d03a718c4709aa7f808a9e4fa51e2e78c456c4
Santiago Martin <santiagomartinagra@gmail.com>
2016-02-29 20:34:34 +0100

    Añadido script que deja la base de datos lista para los test

---
+++ preparar_db_test.sql
@@ -0,0 +1,2 @@
+delete from ttrips where departure_city='oviedo' and departure_wpt_lat='55' and departure_wpt_lon='55';
+delete from tapplications where applicants_id = 306 and appliedtrips_id = 30;
